61bd33d50a85fa58bb5edb9d88fcdebd8039b740
Alvin Phan <alvintp2@uci.edu>
2024-02-07 22:25:32 -0800

    Inline styling for react elements

--- Section36/src/App.js
+++ Section36/src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import React from "react";
 import UploadButton from "./UploadButton.jsx";
 import Title from "./Title.jsx";
+import { customStyle } from "./index.js";
+
 function App() {
   return (
     <div>
       <Title text="ScanSync" size="h1" />
       <UploadButton />
+      <h1 style={customStyle}>This is my application</h1>
     </div>
   );
 }
